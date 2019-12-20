0d6653956ad1d548dec45d96d38e9cd0903f0eff
trevor ngugi <ngugitrevor@gmail.com>
2019-12-20 13:19:06 +0300

    working project

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+# from app import error
+
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -8,4 +11,8 @@ app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')##maybe error here
 
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
 from app import views
+from app import error
